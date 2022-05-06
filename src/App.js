19bba1ed02d94cf0be7cc8172a3597e0520f24a6
Christian John Gonzalez <bournecreative@gmail.com>
2022-05-06 00:31:15 -0700

    initial commit drop-down-select component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { SelectDropDown } from './components/SelectDropDown/index.js'
+
+const options = [
+	{
+		id: 1,
+		label: 'green',
+		value: 'green'
+	},
+	{
+		id: 2,
+		label: 'blue',
+		value: 'blue'
+	},
+	{
+		id: 3,
+		label: 'cyan',
+		value: 'cyan'
+	}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<SelectDropDown options={options} />
+		</div>
+	)
 }
 
-export default App;
+export default App
